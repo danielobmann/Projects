4428abaf6a5b681d6c1c2460d60f2877fec5b783
dansenob <obmann.daniel@live.de>
2020-08-16 17:08:33 +0200

    ADD: plots of results

--- Gambler/gambler.py
+++ Gambler/gambler.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Gambler:
@@ -34,10 +35,58 @@ class Gambler:
             policy[state] = idx
         return policy
 
+    def play_game(self, s0, policy):
+        hist = [s0]
+        while s0 < self.maximum and s0 > 0:
+
+            bet = policy[s0]
+            coin = np.random.choice(2, p=[self.prob, 1 - self.prob])
+
+            if coin == 0:
+                s0 += bet
+            else:
+                s0 -= bet
+            hist.append(s0)
+
+        if s0 == self.maximum:
+            outcome = 1
+        else:
+            outcome = 0
+
+        return outcome, hist
+
+
+
 
 G = Gambler(prob=0.4)
 G.value_iteration(theta=1e-10)
 
-plt.plot(G.get_policy())
+plt.plot(G.get_policy(), '.')
+plt.savefig("Gambler/policy.pdf", format='pdf')
 
 G.evaluate_state(5)
+
+G.play_game(40, G.get_policy())
+
+N = 10000
+policy = G.get_policy()
+
+averages = []
+
+for budget in range(1, 100):
+    outcomes = []
+    for _ in range(N):
+        s0 = budget
+        out, _ = G.play_game(s0, policy=policy)
+        outcomes.append(out)
+
+    averages.append(np.mean(outcomes))
+
+print(averages)
+
+plt.plot(averages, label="Win probability")
+plt.plot(G.values, label="Values of each state")
+plt.legend()
+plt.savefig("Gambler/comparison.pdf", format='pdf')
+
+
